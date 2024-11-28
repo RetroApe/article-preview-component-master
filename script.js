6ccd0e56ba2ae006531e92aa0dda9164379969a6
Tomislav Šuto <113673335+RetroApe@users.noreply.github.com>
2024-11-28 13:34:33 +0100

    Share Pop-up done, but I like it

--- script.js
+++ script.js
@@ -16,18 +16,31 @@ shareBtn.onmouseover = function () {
 }
 
 shareBtn.addEventListener('click', function (event) {
-    // if (this !== event.target) return;
     if (shareWindowDesktop.style.transform === "scale(1)") {
         shareWindowDesktop.style.transform = "scale(0)";
+        shareWindowDesktop.style.opacity = "0";
+
         shareWindowMobile.style.transform = "translateX(100%)";
 
-        shareBtn.style.backgroundColor = "#ECF2F8"
+
+        this.style.backgroundColor = "#ECF2F8"
         shareIcon.style.fill = "#6E8098";
+
+        shareIcon.style.transform = "rotate(360deg)"
     } else {
         shareWindowDesktop.style.transform = "scale(1)";
+        shareWindowDesktop.style.opacity = "1";
+
         shareWindowMobile.style.transform = "translate(0)";
 
-        shareBtn.style.backgroundColor = "#6E8098"
+
+        this.style.backgroundColor = "#6E8098"
         shareIcon.style.fill = "white";
+
+        shareIcon.style.transform = "rotate(360deg)"
     }
 });
+
+shareWindowDesktop.addEventListener('click', (event) => {
+    event.stopPropagation();
+})
